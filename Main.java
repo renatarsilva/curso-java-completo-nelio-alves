16a87e172df006ae56762b8b1df3a5113f1d5025
Renata <renata.rodriguesdasilva@outlook.com>
2026-03-06 23:39:28 -0300

    feat: comandos básicos de saida

--- Main.java
+++ Main.java
@@ -1,2 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual seu nome?");
+        String nome = sc.nextLine();
+
+        System.out.println(nome);
+
+        double x = 10.35784;
+
+        System.out.printf("%.2f \n", x);
+
+        // inverter , para .
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.2f", x);
+
+
+    }
 }
