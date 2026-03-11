01d7ce040ce4d10a271d76b4061d775c99bb1fc2
Renata <renata.rodriguesdasilva@outlook.com>
2026-03-10 23:57:51 -0300

    feat: while

--- modulo07/Whiledois.java
+++ modulo07/Whiledois.java
@@ -1,20 +1,19 @@
 package modulo07;
-
 import java.util.Scanner;
-
-public class While {
+public class Whiledois {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite um numero");
         int x = sc.nextInt();
 
-        int soma = 0;
-        while (x != 0 ){
-            soma += x;
+
+
+        while(x != 79){
+            System.out.println("Digite outro valor");
             x = sc.nextInt();
         }
-        System.out.println(soma);
+        System.out.println("Você acertou!!");
         sc.close();
     }
 }
