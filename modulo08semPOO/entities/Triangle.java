327527d694a1064e0022e0be87d3be76aa016e81
Renata <renata.rodriguesdasilva@outlook.com>
2026-03-14 01:56:28 -0300

    classe Triangle and object

--- modulo08semPOO/entities/Triangle.java
+++ modulo08semPOO/entities/Triangle.java
@@ -4,4 +4,12 @@ public class Triangle {
     public double a;
     public double b;
     public double c;
+
+    //publico para qualquer classe, tipo do dado que esse metodo vai retornar no caso double, nome do metodo, parametros do metodo;
+    public double area(){
+         double p = (a + b + c)/2.0;
+        return Math.sqrt(p*(p-a) * (p-b) * (p-c));
+
+
+    }
 }
